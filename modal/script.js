f3af48a5be6cd8f851ffb54e8678b18e552f2d92
nithesh407 <nitheshravikumar13631@gmail.com>
2023-12-24 13:00:38 +0530

    games using javascript

---
+++ modal/script.js
@@ -0,0 +1,30 @@
+'use strict';
+const allModalsBtn = document.querySelectorAll('.show-modal')
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const closeBtn = document.querySelector('.close-modal')
+
+const closeModal = () => {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+const openModal = () => {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+for (let i = 0; i < allModalsBtn.length; i++) {
+    console.log(allModalsBtn[i].textContent)
+    allModalsBtn[i].addEventListener('click', openModal)
+}
+
+closeBtn.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        closeModal()
+    }
+})
